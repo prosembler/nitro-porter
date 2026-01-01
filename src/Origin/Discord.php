5bd8d95d9d388ba1302db3c41ca2c6bfd891f746
Lincoln Russell <lincoln@icrontic.com>
2026-01-01 17:25:36 -0500

    Add Discord origin iterative message loop, resume, custom rate limiting, and logging

--- src/Origin/Discord.php
+++ src/Origin/Discord.php
@@ -25,6 +25,7 @@
 
 namespace Porter\Origin;
 
+use Porter\Config;
 use Porter\Log;
 use Porter\Migration;
 use Porter\Origin;
@@ -40,7 +41,15 @@ class Discord extends Origin
         'name' => 'Discord',
     ];
 
-    public const int MIN_MICROSECONDS = 20000; // Wait 20 milliseconds (1/50 sec) to force rate limit compliance.
+    /**
+     * Discord has both a 50/sec rate limit (API) and a 10K/10min rate limit (Cloudflare).
+     * 1 second = 1,000 milliseconds = 1,000,000 microseconds.
+     * Limits are therefore 1/20000 microseconds (API) and 1/62500 microseconds (CF).
+     * Because scrapes are likely to exceed 10 minutes, use the slower rate / longer wait.
+     */
+    public const int MIN_MICROSECONDS = 62500; // Wait to force rate limit compliance.
+
+    public const int MICROSECONDS_PER_SEC = 1000000;
 
     public const int MAX_CHANNEL_QUERIES = 10000; // Channels + threads.
 
@@ -61,7 +70,7 @@ class Discord extends Origin
         'joined_at' => 'varchar(100)',
         'premium_since' => 'varchar(100)',
         // Under 'user' object
-        'id' => 'varchar(100)', //@todo key
+        'id' => 'varchar(100)',
         'username' => 'varchar(100)',
         'discriminator' => 'varchar(100)',
         'global_name' => 'varchar(100)',
@@ -69,12 +78,18 @@ class Discord extends Origin
         'email' => 'varchar(100)',
         'bot' => 'tinyint',
         'verified' => 'tinyint',
+        'keys' => [
+            'discord_users_id_primary' => [
+                'type' => 'primary',
+                'columns' => ['id'],
+            ]
+        ],
     ];
 
-    /** @var array|string[]  */
+    /** @var array  */
     protected const array DB_STRUCTURE_CHANNELS = [
-        'id' => 'varchar(100)', //@todo key
-        'type' => 'int', //@todo key
+        'id' => 'varchar(100)',
+        'type' => 'int', //@todo key?
         'guild_id' => 'varchar(100)',
         'position' => 'varchar(100)',
         'name' => 'text',
@@ -86,14 +101,20 @@ class Discord extends Origin
         'owner_id' => 'varchar(100)',
         'member_count' => 'int',
         'thread_metadata' => 'text',
+        'keys' => [
+            'discord_channels_id_primary' => [
+                'type' => 'primary',
+                'columns' => ['id'],
+            ]
+        ],
     ];
 
     /** @var array  */
     protected const array DB_STRUCTURE_MESSAGES = [
-        'id' => 'varchar(100)', //@todo key
-        'channel_id' => 'varchar(100)', //@todo key
+        'id' => 'varchar(100)',
+        'channel_id' => 'varchar(100)', //@todo key?
         'content' => 'text',
-        'timestamp' => 'varchar(100)',
+        'timestamp' => 'varchar(100)', //@todo key?
         'edited_timestamp' => 'varchar(100)',
         'pinned' => 'tinyint',
         'type' => 'int',
@@ -111,10 +132,17 @@ class Discord extends Origin
         'mentions' => 'text',
         'mention_roles' => 'text',
         'mention_channels' => 'text',
+        'keys' => [
+            'discord_messages_id_primary' => [
+                'type' => 'primary',
+                'columns' => ['id'],
+            ]
+        ],
     ];
 
     /**
      * Discord uses 'channel' for ANY type of message container (e.g. a thread) in addition to just 'channel'.
+     * Current behavior is to REBUILD users & channels on every run, but RESUME messages from last ID.
      * @param ?Migration $port
      * @see https://discord.com/developers/docs/topics/rate-limits#rate-limits
      */
@@ -124,17 +152,11 @@ class Discord extends Origin
         $this->input->setHeader('Authorization', 'Bot ' . $this->config['token']);
 
         // Users
-        if ($this->output->exists('discord_users')) {
-            $this->output->getHandle()->table('discord_users')->truncate();
-        }
         $this->users();
 
         // Roles
 
         // Channels
-        if ($this->output->exists('discord_channels')) {
-            $this->output->getHandle()->table('discord_channels')->truncate();
-        }
         $this->channels();
         $channelIds = $this->getTextChannels(); // Before polluting with threads.
         $this->activeThreads();
@@ -142,9 +164,6 @@ class Discord extends Origin
 
         // Messages
         $channelIds = $this->getTextChannels(); // Now including threads.
-        if ($this->output->exists('discord_messages')) {
-            $this->output->getHandle()->table('discord_messages')->truncate();
-        }
         $this->messages($channelIds);
     }
 
@@ -157,25 +176,70 @@ class Discord extends Origin
     }
 
     /**
+     * @see Https::get() for handling of 429s via `retry-after` header.
+     * @param array $headers
+     */
+    private function rateLimit(array $headers, $replySeconds = 0): void
+    {
+        // Get header info.
+        $limit = $headers['x-ratelimit-limit'][0] ?? null; // total number that can be made
+        $remaining = $headers['x-ratelimit-remaining'][0] ?? null; // remaining number that can be made
+        $reset = $headers['x-ratelimit-reset'][0] ?? null; // epoch time
+        $after = $headers['x-ratelimit-reset-after'][0] ?? null; // seconds
+        $bucket = $headers['x-ratelimit-bucket'][0] ?? null; //someID
+        //$scope = $headers['x-ratelimit-scope'][0] ?? null; // user, shared, global
+
+        // Log headers.
+        //Log::comment("lim=$limit, rem=$remaining, res=$reset, aft=$after, bkt=$bucket");
+
+        // Enforce rate limit with sleep.
+        $wait = self::MIN_MICROSECONDS; // min pause to not reach CloudFlare limit.
+        $wait = $wait - (int)($replySeconds * self::MICROSECONDS_PER_SEC); // Synchronous = subtract cycle time.
+        if ($wait < 0) {
+            $wait = 0;
+        }
+        if (!is_null($remaining) && empty($remaining)) { // Zero, not null
+            if (!empty($after) && is_numeric($after)) {
+                $wait = ceil($after) * self::MICROSECONDS_PER_SEC;
+            } else {
+                $wait = 60 * self::MICROSECONDS_PER_SEC; // 1 minute fallback.
+                Log::comment("INFO: Failed to find rate limit info, but limit ($limit) exhausted; pausing 1 minute.");
+            }
+        }
+        usleep($wait);
+    }
+
+    /**
+     * Get channel id list.
      * @return array
      */
     private function getTextChannels(): array
     {
-        // Start timer.
-        $start = microtime(true);
-        Log::comment("Get pulled channel ids...");
+        // Allow config list to override.
+        if (!empty($this->config['extra']['channels']) && count($this->config['extra']['channels'])) {
+            return $this->config['extra']['channels'];
+        }
 
-        // Get channel id list.
+        // Get channels from database.
         $channels = $this->outputQB()->from('discord_channels')
             ->distinct()->limit(10000) // Safety from repeat pulls & invalid data.
             ->whereIn('type', array_keys(self::TEXT_CHANNEL_TYPES))
             ->get('id')->toArray();
-        $ids = array_column($channels, 'id');
-        $memory = memory_get_usage();
+        return array_column($channels, 'id');
+    }
 
-        // Report.
-        Log::storage('GET', 'discord_channels', microtime(true) - $start, count($ids), $memory);
-        return $ids;
+    /**
+     * Get oldest message imported.
+     * @param int $channelID
+     * @return int
+     */
+    private function getLastMessageId(int $channelID): int
+    {
+        $message = $this->outputQB()->from('discord_messages')
+            ->orderBy('timestamp', 'asc')->limit(1)
+            ->where('channel_id', $channelID)
+            ->get('id')->first();
+        return $message->id ?? 0;
     }
 
     /**
@@ -186,7 +250,7 @@ class Discord extends Origin
         $query = ['limit' => '1000'];
         $guildId = $this->getGuildId();
         $map = [
-            'user' => [
+            'user' => [ // obj.param => flatName
                 'id' => 'id',
                 'username' => 'username',
                 'discriminator' => 'discriminator',
@@ -229,16 +293,17 @@ class Discord extends Origin
     {
         foreach ($channelIds as $channelId) {
             $endpoint = "channels/$channelId/threads/archived/public";
-            $this->pull($endpoint, self::DB_STRUCTURE_CHANNELS, 'discord_channels', [], 'threads');
-            usleep(self::MIN_MICROSECONDS); // Kludged rate limit.
+            $info = $this->pull($endpoint, self::DB_STRUCTURE_CHANNELS, 'discord_channels', [], 'threads');
+            $this->rateLimit($info['headers']);
         }
     }
 
     /**
-     * Default `limit` is 50; max is 100. Use `before` to page backwards.
-     * > Returns an array of message objects from newest to oldest on success.
-     * @see https://discord.com/developers/docs/resources/message
-     * @see https://discord.com/developers/docs/resources/message#message-object-message-types (types)
+     * Cycle through channels 1 call each to avoid getting rate limited.
+     *
+     * Discord/Cloudflare seems to interpret as identical GET despite $query changing.
+     * It gives a 5-sec timeout every ~10 "identical" requests, which slows things down badly.
+     * This means you're effectively limited to ~5K messages per minute if you don't cycle channels.
      */
     protected function messages(array $channelIds): void
     {
@@ -247,33 +312,71 @@ class Discord extends Origin
             return;
         }
 
-        foreach ($channelIds as $channelId) {
-            // Setup.
+        // Do not reset data every run.
+        $this->output->protectTable('discord_messages');
+
+        // Status tracking.
+        $channels = array_fill_keys($channelIds, false);
+        $k = 0;
+
+        // Process until channels' status=true.
+        do {
+            $k++;
+            $channels = $this->messageLoop($channels);
+            $remaining = count(array_diff($channels, [true]));
+
+            // Report.
+            $warn = (1 === $remaining) ? " Expect 5-second timeouts." : '';
+            $now = date('H:i:s e');
+            Log::comment("\nINFO: $remaining channels incomplete after pass $k at $now.$warn\n");
+        } while (0 !== $remaining);
+    }
+
+    /**
+     * Do 1 cycle through all channels to get a batch of messages.
+     * Endpoint default `limit` is 50; max is 100. Use `before` to page backwards.
+     *  > Returns an array of message objects from newest to oldest on success.
+     * @see https://discord.com/developers/docs/resources/message
+     * @see https://discord.com/developers/docs/resources/message#message-object-message-types (types)
+     * @param array $channels
+     * @return array
+     */
+    protected function messageLoop(array $channels): array
+    {
+        foreach ($channels as $channelId => $status) {
+            // Check status.
+            if (true === $status) {
+                continue; // Channel is done, bail.
+            } elseif (false === $status) { // Resume?
+                $channels[$channelId] = $this->getLastMessageId($channelId);
+                Log::comment("\nResumed channel $channelId at message {$channels[$channelId]}");
+            }
+
+            // Setup & pull batch.
             $endpoint = "channels/$channelId/messages";
-            $lastMessage = 0;
-            $countBatches = 0;
+            $query = ['limit' => '100'];
+            if (is_numeric($channels[$channelId]) && $channels[$channelId]) {
+                $query['before'] = $channels[$channelId];
+            }
+            $info = $this->pull($endpoint, self::DB_STRUCTURE_MESSAGES, 'discord_messages', $query);
 
-            do {
-                // Pull batch.
-                $countBatches++;
-                $query = ['limit' => '100'];
-                if ($lastMessage) {
-                    $query['before'] = $lastMessage;
-                }
-                $info = $this->pull($endpoint, self::DB_STRUCTURE_MESSAGES, 'discord_messages', $query);
+            // Update status.
+            if (0 === (int)$info['rows']) {
+                // Change status to 'done' if no more rows found.
+                $channels[$channelId] = true;
+                Log::comment("Channel $channelId has no messages past {$channels[$channelId]}, skipping.");
+            } elseif (isset($info['last']['id'])) {
+                // Update offset & report where we are.
+                $id = $channels[$channelId] = $info['last']['id']; // Should be oldest message.
+                $elapsed = formatElapsed($info['api_time']);
+                $time = $info['last']['timestamp'] ?? '';
+                Log::comment("> $elapsed; last message id=$id; timestamp=" . $time);
+            }
 
-                // Next offset.
-                if (isset($info['last']['id'])) {
-                    $lastMessage = $info['last']['id']; // Should be oldest message.
-                } else {
-                    break;
-                }
-
-                // Loop controls.
-                $moreRows = ($info['rows'] > 0); // If rows found, infer not at end.
-                $moreBatchesAllowed = ($countBatches < self::MAX_MESSAGE_QUERIES); // Max/channel sanity check.
-                usleep(self::MIN_MICROSECONDS); // Forced rate limit.
-            } while ($moreRows && $moreBatchesAllowed);
+            // Rate limit.
+            $this->rateLimit($info['headers'], $info['pull_time']);
         }
+
+        return $channels;
     }
 }

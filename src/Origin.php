981d921798f25d9aa4116023c3594242091e13fe
Lincoln Russell <code@lincolnwebs.com>
2026-04-10 11:29:24 -0400

    Fix reversed key logic in Origin

--- src/Origin.php
+++ src/Origin.php
@@ -72,9 +72,9 @@ abstract class Origin extends Package
         // Discard the rest of the content if we only want a key's contents.
         if (!empty($key)) {
             if (!empty($content) && !empty($content[$key])) {
-                Log::comment("Warning: Key '{$key}' not found in response from '{$endpoint}'.");
-            } else {
                 $content = $content[$key];
+            } else {
+                Log::comment("Warning: Key '{$key}' not found in response from '{$endpoint}'.");
             }
         }
 

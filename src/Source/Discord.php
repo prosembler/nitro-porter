bab1cd9e28ef0752a1d3c0b613a20e53d9d2e89c
Lincoln Russell <code@lincolnwebs.com>
2026-05-26 07:15:47 -0400

    Complete v1 Discord source mapping

--- src/Source/Discord.php
+++ src/Source/Discord.php
@@ -24,19 +24,15 @@ class Discord extends Source
             'Discussions' => 1,
             'Comments' => 1,
             'Roles' => 1,
-            // @todo
-            'Avatars' => 0,
-            'Attachments' => 0,
-            'Polls' => 0,
-            'Reactions' => 0,
-            // Not possible.
-            'Passwords' => 0,
-            'AvatarThumbnails' => 0,
-            'PrivateMessages' => 0,
-            'Signatures' => 0,
-            'Bookmarks' => 0,
+            'Avatars' => 1,
+            'Attachments' => 1,
+            'Emoji' => 1,
+            'Reactions' => 0, // No Origin support yet — requires separate calls
+            'Polls' => 0, // No Origin support yet — requires inline unpacking
         ]
     ];
+
+    public const int DISCORD_EPOCH_DIFF = 1288834974657;
 
     public const array CHANNEL_TYPE = [
         'GUILD_TEXT' => 0,
@@ -48,7 +44,7 @@ class Discord extends Source
 
     protected const array FLAGS = [
         'hasDiscussionBody' => false,
-        //'fileTransferSupport' => true,
+        'fileTransferSupport' => true,
         'renumberIndices' => true,  // @todo respect this flag
     ];
 
@@ -57,9 +53,12 @@ class Discord extends Source
         $map = [
             'id' => 'UserID',
             'derived_name' => 'Name', // prefer 1) nick 2) global_name 3) username
+            'derived_avatar' => 'Photo', // prefer guild-specific 'avatar' to 'global_avatar'
+            'joined_at' => 'DateInserted', // Guild-specific date
         ];
         $query = $this->sourceQB()->from('discord_users')->select('discord_users.*')
-            ->selectRaw('COALESCE(nick, COALESCE(global_name, username)) as derived_name');
+            ->selectRaw('COALESCE(nick, COALESCE(global_name, username)) as derived_name')
+            ->selectRaw('COALESCE(avatar, global_avatar) as derived_avatar');
         $this->export('User', $query, $map);
     }
 
@@ -68,6 +67,7 @@ class Discord extends Source
         $map = [
             'id' => 'RoleID',
             'name' => 'Name',
+            //position, managed, mentionable
         ];
         $query = $this->sourceQB()->from('discord_roles')->distinct('id')->select();
         $this->export('Role', $query, $map);
@@ -86,6 +86,10 @@ class Discord extends Source
         $map = [
             'id' => 'CategoryID',
             'name' => 'Name',
+            'parent_id' => 'ParentCategoryID',
+            'position' => 'Sort',
+            'topic' => 'Description',
+            'last_message_id' => 'LastCommentID',
         ];
         $query = $this->sourceQB()->from('discord_channels')->select('discord_channels.*')
             ->whereIn('type', [
@@ -103,12 +107,17 @@ class Discord extends Source
             'name' => 'Name',
             'parent_id' => 'CategoryID',
             'owner_id' => 'InsertUserID',
+            'last_message_id' => 'LastCommentID',
+            'message_count' => 'LastCommentID',
+            'derived_timestamp' => 'DateInserted',
         ];
         $filters = [
             'parent_id' => fn($val, $col, $row) // Text channels use 'id' as 'parent_id' — they are their own category.
-                => (Discord::CHANNEL_TYPE['GUILD_TEXT'] === $row['type']) ? $row['id'] : $row['parent_id']
+                => (Discord::CHANNEL_TYPE['GUILD_TEXT'] === $row['type']) ? $row['id'] : $row['parent_id'],
+            'derived_timestamp' => __NAMESPACE__ . '\Discord::timestampFromSnoflake',
         ];
         $query = $this->sourceQB()->from('discord_channels')->select('discord_channels.*')
+            ->selectRaw('id as derived_timestamp')
             ->whereIn('type', [
                 self::CHANNEL_TYPE['PUBLIC_THREAD'],
                 self::CHANNEL_TYPE['GUILD_ANNOUNCEMENT'],
@@ -124,10 +133,56 @@ class Discord extends Source
             'content' => 'Body',
             'channel_id' => 'DiscussionID',
             'authorid' => 'InsertUserID',
+            'pinned' => 'Announce',
+            //'embeds' => '',
+                // [{"type":"link","url":"http:\/\/www.example.com","description":"Your source for video game news..."}]
         ];
         $query = $this->sourceQB()->from('discord_messages')->select('discord_messages.*')
             ->selectRaw('timestamp(timestamp) as DateInserted')
             ->selectRaw('timestamp(edited_timestamp) as DateUpdated');
         $this->export('Comment', $query, $map);
+    }
+
+    protected function attachments(): void
+    {
+        $map = [
+            'id' => 'MediaID',
+            'message_id' => 'ForeignID',
+            'filename' => 'Name',
+            'width' => 'ImageWidth',
+            'height' => 'ImageHeight',
+            'size' => 'Size',
+            'content_type' => 'Type',
+            'download_path' => 'SourceFullPath',
+        ];
+        $query = $this->sourceQB()->from('discord_attachments')->select('discord_attachments.*');
+        $this->export('Media', $query, $map);
+    }
+
+    protected function emojis(): void
+    {
+        $map = [
+            'id' => 'EmojiID',
+            'name' => 'Name',
+            'animated' => 'Animated',
+            'user.id' => 'InsertUserID',
+        ];
+        $query = $this->sourceQB()->from('discord_emojis')->select('discord_emojis.*');
+        $this->export('Emoji', $query, $map);
+    }
+
+    /**
+     * Discord SnowflakeIDs have timestamps embedded within them.
+     *
+     * @param mixed $value A Discord SnowflakeID
+     * @return int|null Unix timestamp
+     */
+    protected function timestampFromSnoflake(mixed $value): ?int
+    {
+        if (empty($value)) {
+            return null;
+        }
+        $timestamp = substr(decbin((int) $value), 0, -22);
+        return bindec($timestamp) + self::DISCORD_EPOCH_DIFF;
     }
 }

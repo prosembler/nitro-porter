52c83ae451c854292391f7a6f6d7ef3b25e90efd
Lincoln Russell <code@lincolnwebs.com>
2026-07-30 13:14:03 -0400

    Create a StorageInfo data transfer object (DTO)

---
+++ src/StorageInfo.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Porter;
+
+/** DTO for info on workflow steps. */
+final readonly class StorageInfo
+{
+    public function __construct(
+        // Workflow state.
+        public string $name = '',
+        public int $memory = 0,
+        public int $rows = 0,
+        public array $content = [],
+        // Times.
+        public float $startTime = 0,
+        public float $endTime = 0,
+        public float $requestTime = 0,
+        // Http details.
+        public array $headers = [],
+        public string $query = '',
+        public int $http_code = 0,
+        public string $endpoint = '',
+    ) {
+    }
+
+    public function getElapsed(): float
+    {
+        return $this->startTime - $this->endTime;
+    }
+
+    public function getFirst(): mixed
+    {
+        return array_first($this->content);
+    }
+
+    public function getLast(): mixed
+    {
+        return array_last($this->content);
+    }
+}

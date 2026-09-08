141870beff6560dc543918b7125d8e0cce0e5854
Lincoln Russell <code@lincolnwebs.com>
2026-09-08 08:04:41 -0400

    Auto-offset select keys in Target

--- config-sample.php
+++ config-sample.php
@@ -36,6 +36,10 @@ return [
         'discussions' => 0,
         'comments' => 0,
         'attachments' => 0,
+        'polls' => 0,
+        'polloptions' => 0,
+        'tags' => 0, // reactions
+        'badges' => 0,
     ],
 
     // Data connections.
